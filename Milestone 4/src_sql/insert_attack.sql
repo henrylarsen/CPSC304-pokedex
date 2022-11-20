a9d3efa1c4ce063d3df3512123e12f2e636da55f
Henry Larsen <henryl10@student.ubc.ca>
2022-11-20 14:57:27 -0800

    add more inserts

---
+++ Milestone 4/src_sql/insert_attack.sql
@@ -0,0 +1,7 @@
+INSERT INTO attack(AttackName, Type, Damage) VALUES ('Gust', 'Flying', 40);
+INSERT INTO attack(AttackName, Type, Damage) VALUES ('Sand Attack', 'Ground', 0);
+INSERT INTO attack(AttackName, Type, Damage) VALUES ('Horn Attack', 'Normal', 65);
+INSERT INTO attack(AttackName, Type, Damage) VALUES ('Tackle', 'Normal', 40);
+INSERT INTO attack(AttackName, Type, Damage) VALUES ('Peck', 'Flying', 35);
+INSERT INTO attack(AttackName, Type, Damage) VALUES ('Thunder Shock', 'Electric', 40);
+INSERT INTO attack(AttackName, Type, Damage) VALUES ('Hypnosis', 'Psychic', 0);
