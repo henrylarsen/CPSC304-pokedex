d8d6f6d944a72fbf1f2378bb4cf18fb781b275f5
UBC Student <woxtoby@students.cs.ubc.ca>
2022-11-20 14:41:39 -0800

    basics of html_layout

---
+++ Milestone 4/public_html/action_selection.php
@@ -0,0 +1,52 @@
+<html>
+    <head>
+	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    </head>
+<body>
+    <nav class="navbar navbar-default"> 
+        <div class="container-fluid">
+    		<div class="navbar-header">
+      			<a class="navbar-brand" href="./home.php">Pokemon Database</a>
+    		</div>
+    		<ul class="nav navbar-nav">
+				<li><a href="./home.php">Home</a></li>
+				<li><a href="./insert.php">INSERT</a></li>
+				<li><a href="./delete.php">DELETE</a></li>
+				<li><a href="./update.php">UPDATE</a></li>
+				<li class="active"><a href="./selection.php">SELECTION</a></li>
+				<li><a href="./projection.php">PROJECTION</a></li>
+				<li><a href="./join.php">JOIN</a></li>
+				<li><a href="./division.php">DIVISION</a></li>
+				<li class="dropdown">
+					<a class="dropdown-toggle" data-toggle="dropdown" href="#">AGGREGATION<span class="caret"></span></a>
+					<ul class="dropdown-menu">
+					<li><a href="./group_by.php">GROUP BY</a></li>
+					<li><a href="./having.php">HAVING</a></li>
+					<li><a href="./nested.php">NESTED - GROUP BY</a></li>
+					</ul>
+				</li>
+    		</ul>
+        </div>
+    </nav>
+Welcome <?php echo $_POST["id"]; ?><br>
+<?php
+	if ($c=oci_connect("ora_woxtoby", "a24563199", "dbhost.students.cs.ubc.ca:1522/stu")) {
+		echo "Successfully connected to Oracle.\n";
+		oci_close($c);
+		
+		$stid = oci_parse($c, 'SELECT id FROM Pokemon');
+		if (!$stid) {
+			$e = oci_error($c);
+			trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+		}
+		echo $stid;
+	} else {
+		$err = oci_error();
+		echo "Oracle Connect Error " . $err['message'];
+	}
+?>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
