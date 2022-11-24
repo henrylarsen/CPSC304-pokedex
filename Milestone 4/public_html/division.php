a757b7df6f4918119b8e7a9a74607ae7852de90c
UBC Student <woxtoby@students.cs.ubc.ca>
2022-11-24 15:14:08 -0800

    front-end fully functional

--- Milestone 4/public_html/division.php
+++ Milestone 4/public_html/division.php
@@ -28,13 +28,144 @@
     		</ul>
         </div>
     </nav>
-    <form action="./action_division.php" method="post">
+    <form action="./division.php" method="GET">
         <div class="form-group">
-            <label for="name">Name:</label>
-            <input class="form-control" type="text" name="name">
+			<h3>Selects all pokemon with types Grass and Poison</h3>
+			<p>SELECT Pname <br>
+			FROM Pokemon P <br>
+			WHERE NOT EXISTS ( <br>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SELECT T.TName <br>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;FROM type T <br>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;WHERE (T.TName = 'Grass' OR T.TName = 'Poison') AND NOT EXISTS ( <br>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SELECT I.id <br>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;FROM isType I <br>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;WHERE I.id = P.id AND T.TName = I.TName)) <br>
+			</p>
+			<input type="hidden" id="getTableRequest" name="getTableRequest">
+            <input type="submit" value="getTable" name="getTable">
         </div>
-        <button type="submit" class="btn btn-default">Submit</button>
     </form>
+	<?php
+		$success = true;
+		$db_conn = NULL;
+		function debugAlertMessage($message) {
+			global $show_debug_alert_messages;
+
+			if ($show_debug_alert_messages) {
+				echo "<script type='text/javascript'>alert('" . $message . "');</script>";
+			}
+		}
+
+		function executePlainSQL($cmdstr) { //takes a plain (no bound variables) SQL command and executes it
+			//echo "<br>running ".$cmdstr."<br>";
+			global $db_conn, $success;
+
+			$statement = oci_parse($db_conn, $cmdstr);
+			//There are a set of comments at the end of the file that describe some of the OCI specific functions and how they work
+
+			if (!$statement) {
+				echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
+				$e = OCI_Error($db_conn); // For OCIParse errors pass the connection handle
+				echo htmlentities($e['message']);
+				$success = False;
+			}
+
+			$r = oci_execute($statement, OCI_DEFAULT);
+			if (!$r) {
+				echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
+				$e = oci_error($statement); // For OCIExecute errors pass the statementhandle
+				echo htmlentities($e['message']);
+				$success = False;
+			}
+
+			return $statement;
+		}
+
+		function printResult($result) { //prints results from a select statement
+			$listofprojections = ["Pname"];
+			echo "<br>Retrieved data from table isType:<br>";
+			echo "<table border=1>";
+			echo "<tr>";
+			for ($i =0; $i < sizeof($listofprojections); $i++) {
+				echo "<th>" . $listofprojections[$i] . "</th>";
+			}
+			echo "</tr>";
+
+			while ($row = oci_fetch_array($result, OCI_BOTH)) {
+				echo "<tr>";
+				for ($i = 0; $i < sizeof($listofprojections); $i++) {
+					echo "<td>" . $row[$i] . "</td>";
+				}
+				echo "<tr>";
+			}    
+			echo "</table>";
+		}
+
+		function connectToDB() {
+			global $db_conn;
+			$db_conn = oci_connect("ora_woxtoby", "a24563199", "dbhost.students.cs.ubc.ca:1522/stu");
+
+			if ($db_conn) {
+				debugAlertMessage("Database is Connected");
+				return true;
+			} else {
+				debugAlertMessage("Cannot connect to Database");
+				$e = oci_error(); // For OCILogon errors pass no handle
+				echo htmlentities($e['message']);
+				return false;
+			}
+		}
+
+		function disconnectFromDB() {
+			global $db_conn;
+
+			debugAlertMessage("Disconnect from Database");
+			oci_close($db_conn);
+		}
+
+		function handlePokemonProjectRequest() {
+			global $db_conn;
+
+			$bind1 = "'".$_GET['pokeball']."'";
+
+
+			
+			printResult(executePlainSQL("SELECT Pname
+			FROM Pokemon P
+			WHERE NOT EXISTS (
+					SELECT T.TName
+					FROM type T
+					WHERE (T.TName = 'Grass' OR T.TName = 'Poison') AND NOT EXISTS (
+							SELECT I.id
+							FROM isType I
+							WHERE I.id = P.id AND T.TName = I.TName))"));
+			oci_commit($db_conn);
+		}
+
+		function getListofProjections() {
+			$listOfProjections = [];
+				if (isset($_GET['id'])) {
+					array_push($listOfProjections, "ID");
+				}
+				if (isset($_GET['type'])) {
+					array_push($listOfProjections, "TName");
+				}
+			return $listOfProjections;
+		}
+
+		function handleRequest() {
+			if (connectToDB()) {
+				if (array_key_exists('getTable', $_GET)) {
+					handlePokemonProjectRequest();
+				}
+				disconnectFromDB();
+			}
+		}
+
+		if (isset($_GET['getTableRequest'])) {
+			handleRequest();
+		}
+	?>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
--- Milestone 4/public_html/nested.php
+++ Milestone 4/public_html/nested.php
@@ -28,13 +28,124 @@
     		</ul>
         </div>
     </nav>
-    <form action="./action_nested.php" method="post">
+    <form action="./nested.php" method="GET">
         <div class="form-group">
-            <label for="name">Name:</label>
-            <input class="form-control" type="text" name="name">
+			<h3>Select Type Names where > 20 pokemon have type</h3>
+			<p>SELECT P.Psize, COUNT(P.id) <br>
+				FROM Pokemon P<br>
+				WHERE P.id IN (<br>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SELECT T1.id<br>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;FROM isType T1, isType T2<br>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;WHERE T1.id = T2.id and T1.TName != T2.TName)<br>
+				GROUP BY P.Psize;<br>
+			</p>
+			<input type="hidden" id="getTableRequest" name="getTableRequest">
+            <input type="submit" value="getTable" name="getTable">
         </div>
-        <button type="submit" class="btn btn-default">Submit</button>
     </form>
+	<?php
+		$success = true;
+		$db_conn = NULL;
+		function debugAlertMessage($message) {
+			global $show_debug_alert_messages;
+
+			if ($show_debug_alert_messages) {
+				echo "<script type='text/javascript'>alert('" . $message . "');</script>";
+			}
+		}
+
+		function executePlainSQL($cmdstr) { //takes a plain (no bound variables) SQL command and executes it
+			//echo "<br>running ".$cmdstr."<br>";
+			global $db_conn, $success;
+
+			$statement = oci_parse($db_conn, $cmdstr);
+			//There are a set of comments at the end of the file that describe some of the OCI specific functions and how they work
+
+			if (!$statement) {
+				echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
+				$e = OCI_Error($db_conn); // For OCIParse errors pass the connection handle
+				echo htmlentities($e['message']);
+				$success = False;
+			}
+
+			$r = oci_execute($statement, OCI_DEFAULT);
+			if (!$r) {
+				echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
+				$e = oci_error($statement); // For OCIExecute errors pass the statementhandle
+				echo htmlentities($e['message']);
+				$success = False;
+			}
+
+			return $statement;
+		}
+
+		function printResult($result) { //prints results from a select statement
+			$listofprojections = ["P.Psize", "Count(P.id)"];
+			echo "<br>Retrieved data from table Pokemon:<br>";
+			echo "<table border=1>";
+			echo "<tr>";
+			for ($i =0; $i < sizeof($listofprojections); $i++) {
+				echo "<th>" . $listofprojections[$i] . "</th>";
+			}
+			echo "</tr>";
+
+			while ($row = oci_fetch_array($result, OCI_BOTH)) {
+				echo "<tr>";
+				for ($i = 0; $i < sizeof($listofprojections); $i++) {
+					echo "<td>" . $row[$i] . "</td>";
+				}
+				echo "<tr>";
+			}    
+			echo "</table>";
+		}
+
+		function connectToDB() {
+			global $db_conn;
+			$db_conn = oci_connect("ora_woxtoby", "a24563199", "dbhost.students.cs.ubc.ca:1522/stu");
+
+			if ($db_conn) {
+				debugAlertMessage("Database is Connected");
+				return true;
+			} else {
+				debugAlertMessage("Cannot connect to Database");
+				$e = oci_error(); // For OCILogon errors pass no handle
+				echo htmlentities($e['message']);
+				return false;
+			}
+		}
+
+		function disconnectFromDB() {
+			global $db_conn;
+
+			debugAlertMessage("Disconnect from Database");
+			oci_close($db_conn);
+		}
+
+		function handlePokemonProjectRequest() {
+			global $db_conn;			
+			printResult(executePlainSQL("SELECT P.Psize, COUNT(P.id)
+										FROM Pokemon P
+										WHERE P.id IN (
+											SELECT T1.id
+											FROM isType T1, isType T2
+											WHERE T1.id = T2.id and T1.TName != T2.TName)
+										GROUP BY P.Psize"));
+			oci_commit($db_conn);
+		}
+
+		function handleRequest() {
+			if (connectToDB()) {
+				if (array_key_exists('getTable', $_GET)) {
+					handlePokemonProjectRequest();
+				}
+				disconnectFromDB();
+			}
+		}
+
+		if (isset($_GET['getTableRequest'])) {
+			handleRequest();
+		}
+	?>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
